9235c9a6c36142d9009438d36d7151243ef0616f
sandhyrajawat <sandhyarajawat1100@gmail.com>
2025-07-06 17:58:17 +0530

    api

--- resources/views/ApiDemo.blade.php
+++ resources/views/ApiDemo.blade.php
@@ -4,29 +4,22 @@
         print_r($api);
     }}
     </pre> -->
-    <!--   [17] => Array
-                (
-                    [source] => Array
-                        (
-                            [id] => the-verge
-                            [name] => The Verge
-                        )
+  
 
-                    [author] => Jess Weatherbed
-                    [title] => PCIe 7.0 is coming, but not soon, and not for you
-                    [description] => The PCIe 7.0 specification has now been released, while many of us are still waiting for PCIe 6.0 to materialize in consumer products. The PCI Special Interest Group (PCI-SIG) announced on Wednesday that PCIe 7.0 is now available to members of its organizatio…
-                    [url] => https://www.theverge.com/news/686560/pcie-7-0-specification-launch-bandwith-speed
-                    [urlToImage] => https://platform.theverge.com/wp-content/uploads/sites/2/2025/03/STKS505_QUANTUM_COMPUTING_A.jpg?quality=90&strip=all&crop=0%2C10.732984293194%2C100%2C78.534031413613&w=1200
-                    [publishedAt] => 2025-06-13T09:22:06Z
-                    [content] => PCIe 7.0 is earmarked for data-heavy applications rather than consumer computing hardware. -->
-
-
+@vite('resources/css/app.css')
 @foreach($api as $apis)
-    <h1>{{ $apis['author'] }}</h1>
-    <p>{{ $apis['title'] }}</p>
-    <img src="{{ $apis['urlToImage'] }}" alt="Image">
-    <p>{{ $apis['description'] }}</p>
-    <a href="{{ $apis['url'] }}" target="_blank">Read more</a>
+<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden p-4 space-y-4">
+    <h1 class="text-xl font-semibold text-blue-600">{{ $apis['author'] }}</h1>
+
+    <p class="text-lg font-medium text-gray-800">{{ $apis['title'] }}</p>
+
+    <img src="{{ $apis['urlToImage'] }}" alt="Image" class="w-full h-48 object-cover rounded-md">
+
+    <p class="text-gray-600">{{ $apis['description'] }}</p>
+
+    <a href="{{ $apis['url'] }}" target="_blank" class="inline-block text-sm text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded transition">Read more</a>
+</div>
+
 @endforeach
 
 
