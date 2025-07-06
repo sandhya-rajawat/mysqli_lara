0bf5dd3a41e62731a27ba835f081fa2d3e9b764e
sandhyrajawat <sandhyarajawat1100@gmail.com>
2025-07-06 18:40:23 +0530

    DB_Query

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class QueryController extends Controller
 {
-    function query(){
-        return 'this is my query run right now';
+    function query()
+    {
+        // return DB::select("SELECT * FROM demotable");
+        // return DB::table("demotable")->get();
+        // return 'this is my query run right now';
+        //         $result=DB::table('demotable')->insert([
+        // 'id'=>3,
+        // 'name'=>"priyanka",
+        // "city"=>"bikaner"
+        // $result = DB::table('demotable')->where('name', 'sandhya')->update(['city'=> 'Jodhpur']);
+        $result = DB::table('demotable')->where('name', 'sandhya')->delete();
+        if ($result) {
+            echo "your Data delete Successfull";
+        } else {
+            echo "Somthing error";
+        }
     }
 }
